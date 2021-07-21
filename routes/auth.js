248c92a5e912d04af4ca016b528ebd0e2d53925f
unknown <arian.angoma@tecsup.edu.pe>
2021-07-20 23:48:56 -0500

    SignIn

--- routes/auth.js
+++ routes/auth.js
@@ -5,12 +5,14 @@ const {login, googleSignIn, renewToken} = require("../controllers/auth");
 
 const router = Router();
 
+// Inicio de sesión
 router.post('/login', [
     check('email', 'El correo es obligatorio').isEmail(),
     check('password', 'El contraseña es obligatorio').not().isEmpty(),
     validateFields
 ], login);
 
+// Inicio de sesion por google
 router.post('/google', [
     check('id_token', 'El id_token es necesario').not().isEmpty(),
     validateFields
